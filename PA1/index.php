69bd52f99b690a559c29a3f072747e286c265361
unknown <iguest@is-m440m27-13.ischool.uw.edu>
2014-01-15 19:47:10 -0800

    Done with basics

--- PA1/index.php
+++ PA1/index.php
@@ -1,11 +1,10 @@
 <html>
-	<head>
-	
+	<head>	
 		<title>NBA Player Stats</title>
 		<meta charset="UTF-8">
 		<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
 		<link rel="stylesheet" href="css/styles.css">
-		<link rel="shortcut icon" href="img/logo-vertical.png">
+		<link rel="shortcut icon" href="img/nbaLogo.png">
 		<meta name="author" content="1024261">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	
@@ -15,11 +14,10 @@
 			<img src="img/nba.jpg" alt="NBA" class="sized center-block img-rounded">
 			
 			<form class="center-block" action="result.php" Method="post">
-				<input class="center-block" name="name" Type="text">
+				<input class="center-block" name="name" type="text">
 				<input class="center-block" type="submit" value="Search">
-			
-			
 			</form>
+			
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 			<script src="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 		</div>
