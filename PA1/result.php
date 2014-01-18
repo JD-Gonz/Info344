1ecb345e40df3e5a6f390ff9aced798162e9a1b9
unknown <iguest@is-m430m27-38.ischool.uw.edu>
2014-01-17 17:32:12 -0800

    think i got the basics done

--- PA1/result.php
+++ PA1/result.php
@@ -1,25 +1,52 @@
 <?php
 $name = $_POST["name"];
-
-
+$username = "info344user";
+$password = "Godspeed23";
 
 // Connects to your Database
-mysql_connect("info344.cf9rvll9cbvo.us-west-2.rds.amazonaws.com:3306", "info344user", "Godspeed23") or die(mysql_error());
-mysql_select_db("innodb") or die(mysql_error());
-
-
-
-$results = mysql_query("SELECT * FROM PlayerStats WHERE PlayerName like '%$name%'");
-
-while ($row = mysql_fetch_assoc($results)) {
-    echo $row["PlayerName"] . " ";
-    echo $row["GP"] . " ";
-    echo $row["FGP"] . " ";
-    echo $row["TPP"] . " ";
-    echo $row["FTP"] . " ";
-    echo $row["PPG"] . "<br>";
-    
+try {
+	$conn = new PDO('mysql:host=info344.cf9rvll9cbvo.us-west-2.rds.amazonaws.com:3306;dbname=innodb', $username, $password);
+	$stmt = $conn->prepare("SELECT * FROM PlayerStats WHERE PlayerName like '%$name%'");
+	$stmt -> execute(array());
+	
+	$result = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+	
+	if ( count($result) ) {
+		foreach($result as $row) {
+			 print_r($row);
+			 print("\n");
+		}
+	} else {
+		echo "No rows returned.";
+	}
+} catch(PDOException $e) {
+	echo 'ERROR: ' . $e->getMessage();
 }
-
+?>
+<html>
+<head>
+<title>Results for $name</title>
+</head>
+<body>
+<?php 
 
 ?>
+</body>
+</html>
+
+
+<!--  Perform database query -->
+<!--  $query = "SELECT * FROM PlayerStats WHERE PlayerName like '%$name%'"; -->
+<!--  $results = mysql_query($query) or die('Query failed: ' . mysql_error());; -->
+
+<!--  while ($row = mysql_fetch_assoc($results)) { -->
+<!--      echo $row["PlayerName"] . " "; -->
+<!--      echo $row["GP"] . " "; -->
+<!--      echo $row["FGP"] . " "; -->
+<!--      echo $row["TPP"] . " "; -->
+<!--      echo $row["FTP"] . " "; -->
+<!--      echo $row["PPG"] . "<br>"; -->
+    
+<!--  } -->
+
+
